8a414aca192819351fd12181cd7a34dcbd4ae586
teagueStockwell <tsAppDevelopment@gmail.com>
2021-07-30 13:05:40 -0700

    client side errors

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -2,24 +2,24 @@ import errorHandler from 'express-mung'
 import {Response, Request} from 'express'
 
 // given each res, when they dont have a body, transform the res before sending
-const handle = (body, req: Request, res: Response) => {
+const handle = (body, _req: Request, res: Response) => {
   try {
     if (!body) {
       if (res.statusCode === 200) {
-        return {msg: `200: ${req.method} @ ${req.originalUrl}`}
+        return {msg: `200: Successful`}
       }
       if (res.statusCode === 400) {
         return {
-          msg: `400: Name must be unique to ${req.method} @ ${req.originalUrl}`,
+          msg: `400: Invalid input, name must be unique`,
         }
       }
       if (res.statusCode === 403) {
         return {
-          msg: `403: You do not have a high enough role to ${req.method} @ ${req.originalUrl}`,
+          msg: `403: You're not allowed to do that`,
         }
       }
       return {
-        msg: `500: We cant ${req.method} @ ${req.originalUrl}. Please refresh the previous screen`,
+        msg: `500: Error please refresh`,
       }
     }
   } catch (e) {
