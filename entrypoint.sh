c8f7291e84a0ebf12a75283774cbb6a203a45c35
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-07 21:31:04 -0700

    reseed deep hash id

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ echo "waiting for DB to accept conenctions: 3" && node -e "(() => {setTimeout(()
 echo "waiting for DB to accept conenctions: 2" && node -e "(() => {setTimeout(() => {return}, 1000)})()"
 echo "waiting for DB to accept conenctions: 1" && node -e "(() => {setTimeout(() => {return}, 1000)})()"
 
-if [ "${IS_LOCAL}" = "true" ]; then echo "reseting db and reseeding" && npx prisma generate && npx prisma db push --force-reset && node prisma/reseed.js ; else echo "applying migrations prod" && npx prisma generate && npx prisma migrate deploy ; fi
+# if [ "${IS_LOCAL}" = "true" ]; then echo "reseting db and reseeding" && npx prisma generate && npx prisma db push --force-reset && node prisma/reseed.js ; else echo "applying migrations prod" && npx prisma generate && npx prisma migrate deploy ; fi
 
 # # force reseeds for prod
-# npx prisma db push --force-reset && node prisma/reseed.js
+npx prisma db push --force-reset && node prisma/reseed.js
 
 echo "starting server"
 node server.js
