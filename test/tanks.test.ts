150a5010fb550ca115306a61695651354f18553e
teagueStockwell <tsAppDevelopment@gmail.com>
2021-10-07 12:20:24 -0700

    Bump cargo and tank tests

--- test/tanks.test.ts
+++ test/tanks.test.ts
@@ -6,6 +6,8 @@ import assert from 'assert'
 import server from '../server'
 import {Tank} from '@prisma/client'
 import {client as prisma} from '../prisma/client'
+import { O_NOCTTY } from 'constants'
+import { connected } from 'process'
 
 // READ
 describe('GET /tank', () => {
@@ -42,8 +44,8 @@ describe('PUT /tank', () => {
     aircraftId: 1,
     tankId: 1,
     name: 'update tank name',
-    simpleMomsCSV: 'update info here',
-    weightsCSV: '123 I love pe',
+    simpleMomsCSV: '1,2,3',
+    weightsCSV: '1,2,3',
   }
 
   const updateTankNonUnique: Tank = {
@@ -52,8 +54,8 @@ describe('PUT /tank', () => {
     aircraftId: 1,
     tankId: 1, // this is tank 1
     name: 'Tank 3 ER', // tank 1 cant be the same as tanks 3
-    simpleMomsCSV: 'update info here',
-    weightsCSV: '123 I love pe',
+    simpleMomsCSV: '1,2,3',
+    weightsCSV: '1,2,3',
   }
 
   const newTank: Tank = {
@@ -62,8 +64,8 @@ describe('PUT /tank', () => {
     aircraftId: 1,
     tankId: 0, // 0 means this is a new tank - no aircraftId has been assined by db
     name: 'new tank name',
-    simpleMomsCSV: 'info here',
-    weightsCSV: '123 I love pe',
+    simpleMomsCSV: '1,2,3',
+    weightsCSV: '1,2,3',
   }
 
   before(async () => {
@@ -115,6 +117,23 @@ describe('PUT /tank', () => {
       .put('/tank')
       .set('authorization', role3e)
       .send(updateTankNonUnique)
+      .expect(400)
+      .end(done)
+  })
+
+  it('Should return 400 where tank weights.length !== simple moments.length', (done: Done) => {
+    req(server)
+      .put('/tank')
+      .set('authorization', role3e)
+      .send({
+        updatedBy: 'developer',
+        updated: new Date(),
+        aircraftId: 1,
+        tankId: 0, // 0 means this is a new tank - no aircraftId has been assined by db
+        name: 'new tank name',
+        simpleMomsCSV: 'info here',
+        weightsCSV: '123 I love pe',
+      })
       .expect(400)
       .end(done)
   })
